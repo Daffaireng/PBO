085d62c65ea937a6a27acce916d1959dd0312c18
Daffa Louisenbairn <firmansyah.21012006@gmail.com>
2025-09-17 20:46:13 +0700

    Add files via upload

---
+++ nigas/src/main/java/com/mycompany/nigas/Nigas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nigas;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Nigas {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Muafifi Daffa Firmansyah");
+        System.out.println("NPM     : 2440506073");
+        System.out.println("Alamat  : Semalen, Ngadirojo, Secang, Magelang");
+        System.out.println("No_HP   : 0895619811925");
+    }
+}
